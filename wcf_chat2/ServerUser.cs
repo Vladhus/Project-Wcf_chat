891f54e64143c0c14270ff711a54ce41de912e8d
vladtop1999 <vladhusaiov@gmail.com>
2019-04-23 15:32:23 +0200

    TDP - 5 Add System.ServiceModel

--- wcf_chat2/ServerUser.cs
+++ wcf_chat2/ServerUser.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ServiceModel;
 
 namespace wcf_chat2
 {
     public class ServerUser
     {
-        public int MyProperty { get; set; }
+        public int ID { get; set; }
+        public string name { get; set; }
+
+
     }
 }

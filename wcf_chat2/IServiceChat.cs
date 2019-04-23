2a82ed8104235cb77919efb246b4d19e318163ce
vladtop1999 <vladhusaiov@gmail.com>
2019-04-23 15:09:21 +0200

    TDP-1 Add WCF Interfase

---
+++ wcf_chat2/IServiceChat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace wcf_chat2
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IServiceChat" в коде и файле конфигурации.
+    [ServiceContract]
+    public interface IServiceChat
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
